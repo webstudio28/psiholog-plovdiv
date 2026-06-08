a160b8f7da4072bd23f3ed68b6b0a5c307fbce40
andongoshev <andon.go6ev@gmail.com>
2026-06-08 12:30:29 +0300

    new article body stress added

--- src/_data/blogsPublished.js
+++ src/_data/blogsPublished.js
@@ -1,4 +1,11 @@
 const blogs = require("./blogs.json");
 
-/** Posts where `publish === false` are excluded from the site build and listings. */
-module.exports = blogs.filter((b) => b.publish !== false);
+/** Posts where `publish === false` are excluded; sorted newest first by date. */
+module.exports = blogs
+  .filter((b) => b.publish !== false)
+  .sort((a, b) => {
+    const dateDiff = new Date(b.date) - new Date(a.date);
+    if (dateDiff !== 0) return dateDiff;
+    // Same date: later entries in blogs.json win (most recently published)
+    return blogs.indexOf(b) - blogs.indexOf(a);
+  });
